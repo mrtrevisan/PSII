b24128a3bc971c41b341f9cdb5d677892db37333
mrtrevisan <mauroroberto.trevisan@gmail.com>
2023-09-09 16:50:31 -0300

    delete php

--- php/main.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url = 'https://www.ufsm.br/wp-json/wp/v2/eventos';
-$url2 = 'https://jsonplaceholder.typicode.com/posts';
-$json = file_get_contents($url2);
-$data = json_decode($json);
-
-var_dump($data)
-?>
