cb90b511d96b906b9eb8a6888562bc5c2ff50d9c
mrtrevisan <mauroroberto.trevisan@gmail.com>
2023-09-13 21:10:26 -0300

    centros

--- SQL/script.sql
+++ SQL/script.sql
@@ -34,4 +34,19 @@ CREATE TABLE evento (
     nome VARCHAR(255),
     link VARCHAR(255)
 );
+
+CREATE TABLE centro (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    acronimo VARCHAR(10),
+    longitude FLOAT,
+    latitude FLOAT
+);
+
+INSERT INTO centro (nome, longitude, latitude)
+VALUES ('Centro de Tecnologia', 'CT', -29.713190291317783, -53.71682910622894),
+       ('Centro de Ciêcnias da Saude', 'CCS', -29.71357791320489, -53.71387518235088),
+       ('Centro de Ciências Naturais e Exatas', 'CCNE', -29.71448879726987, -53.716452165560874),
+       ('Centro de Artes e Letras', 'CAL', -29.718505197133915, -53.71581290533199),
+       ('Centro de Ciências Sociais e Humanas', 'CCSH', -29.72061309679607, -53.71865958633839)
 */
--- nodejs/backend.js
+++ nodejs/backend.js
@@ -45,8 +45,24 @@ async function connect(){
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
 
 app.get('/', async function(req, res){
+    res.send('UfsmGO');
+})
+
+app.get('/centro', async function(req, res){
     var client = await connect();
-    var query = "SELECT * FROM evento"
+    var query = "SELECT * FROM centro";
+    client.query(query, function(err, result){
+        if(err) {
+            return console.error('error running query', err);
+        }
+        client.release();
+        res.send(result.rows);
+    })
+})
+
+app.get('/evento', async function(req, res){
+    var client = await connect();
+    var query = "SELECT * FROM evento";
     client.query(query, function(err, result){
         if(err) {
             return console.error('error running query', err);
