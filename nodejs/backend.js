48edec813a4713fdb31629cca41f16e0fbc45da6
mrtrevisan <mauroroberto.trevisan@gmail.com>
2023-09-18 10:48:36 -0300

    docker muito gamer

--- nodejs/backend.js
+++ nodejs/backend.js
@@ -4,31 +4,6 @@ dotenv.config();
 const express = require('express')
 const app = express()
 
-/*
-const mysql = require('mysql2')
-var con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_KEY,
-    port: process.env.DB_PORT
-});
-
-function connect() {
-    con.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-    });
-}
-app.get ('/', function (req, res) {
-    connect();
-    var sql = "SELECT * FROM ufsmgo.evento";
-    con.query(sql, function (err, result) {
-        if (err) console.log(err);
-        res.send(result);
-    });
-})
-*/
-
 const {Pool} = require('pg');
 
 async function connect(){
@@ -83,5 +58,17 @@ app.get('/evento', async function(req, res){
         }
         client.release();
         res.send(result.rows);
+    })
+})
+
+app.get('/healthcheck', async function(req, res){
+    var client = await connect();
+    var query = "SELECT NOW() as time";
+    client.query(query, function(err, result){
+        if(err) {
+            return console.error('error running query', err);
+        }
+        client.release();
+        res.status(200).send('ok')
     })
 })
--- nodejs/fetch.js
+++ nodejs/fetch.js
@@ -1,45 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-//MySQL
-/*
-const mysql = require('mysql2')
-
-var con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_KEY,
-    port: process.env.DB_PORT
-});
-
-
-function connect() {
-    con.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-    });
-}
-
-function saveDataMYSQL(data) {
-    connect();
-    data.forEach(element => {
-        var sql = "INSERT INTO ufsmgo.evento (id, data_inicio, data_termino, localizacao, nome, link) VALUES ('"
-                   + element.id + "', '" + element.acf.evento_inicio + "', '" + element.acf.evento_termino + 
-                   "', '" + element.acf.evento_local + "', '" + element.acf.evento_nome + 
-                   "', '" + element.link + "')";
-        console.log(sql);
-        con.query(sql, function (err, result) {
-            if (err) console.log(err);
-            console.log("1 record inserted");
-        });
-    });
-    con.end();
-    console.log("Fim do programa");
-}
-*/
-
-//PostgreSQL
-
 const {Pool} = require('pg');
 
 async function connect(){
@@ -53,7 +14,6 @@ async function connect(){
     }
 }    
 
-    
 async function saveData(data){
     var client = await connect();
 
