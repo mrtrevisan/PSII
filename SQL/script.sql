156d18feef861231928bdb68bf7c7b084dcfa2ee
mrtrevisan <mauroroberto.trevisan@gmail.com>
2023-09-20 17:03:37 -0300

    fetch gamer

--- SQL/script.sql
+++ SQL/script.sql
@@ -1,36 +1,11 @@
---reseta a database
-/*
-DROP database IF EXISTS ufsmgo;
-create database ufsmgo;
-*/
-
-use ufsmgo;
-
---reseta a table
-/*
-DROP table IF EXISTS evento;
-create table evento (
-	id integer,
-    data_inicio varchar(255),
-    data_termino varchar(255),
-    localizacao varchar(255),
-    nome varchar(255),
-    link varchar(255),
-    primary key(id)
-) default charset = utf8mb4;
-*/
-
-select * from evento;
-
-
-
 --postgresql
 /*
 CREATE TABLE evento (
     id INTEGER PRIMARY KEY,
-    data_inicio VARCHAR(255),
-    data_termino VARCHAR(255),
-    localizacao VARCHAR(255),
+    data_inicio DATE,
+    data_termino DATE,
+    local VARCHAR(255),
+    centro VARCHAR(5),
     nome VARCHAR(255),
     link VARCHAR(255)
 );
@@ -38,21 +13,18 @@ CREATE TABLE evento (
 CREATE TABLE centro (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255),
-    acronimo VARCHAR(10),
+    sigla VARCHAR(5),
     longitude FLOAT,
     latitude FLOAT
 );
 
-INSERT INTO centro (nome, longitude, latitude)
+INSERT INTO centro (nome, sigla, longitude, latitude)
 VALUES ('Centro de Tecnologia', 'CT', -29.713190291317783, -53.71682910622894),
        ('Centro de Ciêcnias da Saude', 'CCS', -29.71357791320489, -53.71387518235088),
        ('Centro de Ciências Naturais e Exatas', 'CCNE', -29.71448879726987, -53.716452165560874),
        ('Centro de Artes e Letras', 'CAL', -29.718505197133915, -53.71581290533199),
-       ('Centro de Ciências Sociais e Humanas', 'CCSH', -29.72061309679607, -53.71865958633839)
-
-
-INSERT INTO centro (nome, acronimo, longitude, latitude)
-VALUES ('Centro de Ciências Rurais', 'CCR', -29.7184521203052, -53.71677597449196),
+       ('Centro de Ciências Sociais e Humanas', 'CCSH', -29.72061309679607, -53.71865958633839),
+       ('Centro de Ciências Rurais', 'CCR', -29.7184521203052, -53.71677597449196),
        ('Centro de Educação', 'CE', -29.714884683637273, -53.71807585041829),
        ('Centro de Educação Física e Desportos', 'CEFD', -29.719797995887717, -53.71041164514275),
        ('Colégio Politécnico', 'POLI', -29.722189325079345, -53.71793839904378),
