4fd876c56e8eea71299cdef51ed27508c3e34edb
Ramon <ramonizidoro22@gmail.com>
2023-09-26 13:37:07 -0300

    testando html

--- nodejs/utils.js
+++ nodejs/utils.js
@@ -2,3 +2,10 @@ function doSomething(params) {
     document.getElementById("demo").innerHTML = "Hello World";
     document.getElementById("demo").hidden = !document.getElementById("demo").hidden;
 }
+
+function getPoints(){
+    return parseInt(localStorage.getItem('pontuacao')) || 0;
+}
+var inputElement = document.getElementById("pontuacao");
+var valorDaFuncao = getPoints();
+inputElement.value = valorDaFuncao;
